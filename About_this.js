ae714e5ba0de8572560bcbc1cdee9923abc844eb
hanxbeen <whgksqls13@naver.com>
2021-08-15 12:20:06 +0900

    study : 'this가 같은 곳을 바라보게 하는 방법'

--- About_this.js
+++ About_this.js
@@ -15,3 +15,34 @@ var obj = {
 obj.setValue();
 console.log(value);
 console.log(obj.value);
+
+// 자 근데 this가 서로 다르다보니까 개발할 때 신경써야할 부분이 많은건데...
+// 이 두 this를 미리 인지하고 같게 할 수 있으면 좋겠죠 ?
+
+// 그래서 이 두가지 방법을 소개하고합니다.
+
+var value = 0;
+var obj = {
+  value: 1,
+  setValue: function () {
+    this.value = 2;
+    self = this;
+    (function () {
+      self.value = 3;
+    })();
+  },
+};
+
+///////////////////////////////
+
+var value = 0;
+var obj = {
+  value: 1,
+  setValue: function () {
+    this.value = 2;
+    (function () {
+      this.value = 3;
+    }.call(this));
+    //call 이나 apply로 this를 전달해주면 된다.
+  },
+};
